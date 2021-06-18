d7235dcdae6992a2db1cfa62eff220551fcd73df
Thomas Bielawski <tfbielawski@protonmail.com>
2021-06-18 12:07:36 -0400

    Completed callback tasks. Advancing to Prototypes

--- index.js
+++ index.js
@@ -196,9 +196,9 @@ function greeting(firstName, lastName){return `Hello ${firstName} ${lastName}, n
   
  /* 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁 */ 
  // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
- // console.log(consume(2, 2, add)); // 4
- // console.log(consume(10, 16, multiply)); // 160
- // console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+ console.log(consume(2, 2, add)); // 4
+ console.log(consume(10, 16, multiply)); // 160
+ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
   
   
 // 🦁💪 Stretch: If you haven't already, convert your array method callbacks into arrow functions - make sure you comment out this section before you submit your work 🦁💪
